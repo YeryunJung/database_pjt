c3ebbf49c8d9cf9e5eda4657a0e270aa9a910438
yeryunJung <2bites@naver.com>
2023-04-14 14:47:39 +0900

    update movie_model

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
