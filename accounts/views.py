9f2f8cd07b3fe9e67490c574ca0afbd1f0f0e70b
Gobro <gobro6366@gmail.com>
2023-04-14 15:39:43 +0900

    Implement Signup and add signup button in login.html

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
@@ -22,3 +23,16 @@ def login(request):
 def logout(request):
     if auth_logout(request):
         return redirect("movies:index")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # 회원가입 하면 자동 로그인하도록
+            return redirect("movies:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
