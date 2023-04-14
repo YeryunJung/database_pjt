18ce0b34bcf6ab283a55eb36eaab8dc3cb615ae0
yeryunJung <2bites@naver.com>
2023-04-14 15:13:04 +0900

    update comment class and movie_lie_users

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 class Movie(models.Model):
     title = models.CharField(max_length=20)
@@ -11,8 +11,12 @@ class Movie(models.Model):
     
 class Comment(models.Model):
     content = models.CharField(max_length=100)
-    description = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
+    
+class Movie_like_users(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
