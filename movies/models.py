133c6ddcbeac07959c3778b83714dc3b23b76bb0
yeryunJung <2bites@naver.com>
2023-04-14 15:02:30 +0900

    create comment class

--- movies/models.py
+++ movies/models.py
@@ -8,3 +8,11 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    description = models.TextField()
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
